a663e040789bb057adbfef55d601075d09053484
mika tiihonen <koneautomaatio@hotmail.com>
2024-05-07 14:08:32 +0300

    last commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,121 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <h1>To Do -lista</h1>
+        <ToDoForm @todo-added="addToDo"></ToDoForm>
+        <h2 id="list-summary" ref="listSummary" tabindex="-1">{{ listSummary }}</h2>
+        <ul :aria-labelledby="list-summary" class="stack-large">
+          <li v-for="item in ToDoItems" :key="item.id">
+          <ToDoItem
+              :label="item.label"
+              :done="item.done"
+              :id="item.id"
+              @checkbox-changed="$event => updateDoneStatus(item.id)"
+              @item-deleted="$event => deleteToDo(item.id)"
+              @item-edited="$event => editToDo(item.id, $event)">
+          </ToDoItem>
+          </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
+import ToDoForm from "./components/ToDoForm.vue";
 
 export default {
-  name: 'App',
+  name: 'App', 
   components: {
-    HelloWorld
-  }
+   ToDoItem,
+   ToDoForm
+},
+data() {
+  return {
+  ToDoItems : [
+        { id: uniqueId("todo-"), label: "Opiskele Vue.js", done: false },
+        { id: uniqueId("todo-"), label: "Tee eka Vue.js app", done: true },
+        { id: uniqueId("todo-"), label: "Pidä kivaa", done: true},
+        { id: uniqueId("todo-"), label: "Tee To Do -lista", done: false},
+        { id: uniqueId("todo-"), label: "Ota opintopisteet kotiin", done: false},
+      ],
+    };
+  },
+methods: {
+  addToDo(toDoLabel) {
+    this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
+  },
+  updateDoneStatus(toDoId) {
+      const toDoUpdate = this.ToDoItems.find((item) => item.id === toDoId);
+      toDoUpdate.done = !toDoUpdate.done;    
+    },
+  deleteToDo(toDoId) {
+      const itemIndex = this.ToDoItems.findIndex((item) => item.id === toDoId);
+      this.ToDoItems.splice(itemIndex, 1);
+      this.$refs.listSummary.focus();
+  },
+  editToDo(toDoId, newLabel) {
+      const ToDoEdit = this.ToDoItems.find((item) => item.id === toDoId);
+      ToDoEdit.label = newLabel;
+  },
+  },
+  computed: {
+    listSummary() {
+      const numberFinishedItems = this.ToDoItems.filter(
+        (item) => item.done
+      ).length
+      return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`;
+    },
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  .stack-small > * + * {
+    margin-top: 1.25rem;
+  }
+  .stack-large > * + * {
+    margin-top: 2.5rem;
+  }
+  @media screen and (min-width: 550px) {
+    .stack-small > * + * {
+      margin-top: 1.4rem;
+    }
+    .stack-large > * + * {
+      margin-top: 2.8rem;
+    }
+  }
+  /* End global styles */
+  #app {
+    background: #fff;
+    margin: 2rem 0 4rem 0;
+    padding: 1rem;
+    padding-top: 0;
+    position: relative;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+  }
+  @media screen and (min-width: 550px) {
+    #app {
+      padding: 4rem;
+    }
+  }
+  #app > * {
+    max-width: 50rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  #app > form {
+    max-width: 100%;
+  }
+  #app h1 {
+    display: block;
+    min-width: 100%;
+    width: 100%;
+    text-align: center;
+    margin: 0;
+    margin-bottom: 1rem;
+  }
 </style>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <form @submit.prevent="onSubmit">
+  <h2 class="label-wrapper">
+    <label for="todo-input" class="input-lg">Mitä pitäisi tehdä? </label>
+  </h2>
+    <input
+      type="text"
+      id="todo-input"
+      name="new-todo"
+      autocomplete="off" 
+       v-model.lazy.trim="label"
+       class="input-lg"/>
+    <button type="submit" class="btn btn-primary btn-lg" style="margin: 20px;" >Lisää</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    methods: {
+      onSubmit() {
+        if (this.label === "") {
+          return;
+        }
+        this.$emit("todo-added", this.label);
+        this.label = "";
+      },
+    },
+    data () {
+      return {
+        label: "",
+      };
+    },
+  };
+</script>
+
+<style>
+  button {
+    margin: 10px;
+    padding: 10px;
+  }
+</style>
